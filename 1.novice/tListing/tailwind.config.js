903363833d180e75b3ca905c8e8953d58bfe53b9
un4ckn0wl3z <slickerth@gmail.com>
2022-05-05 22:31:18 +0700

    add simple page

--- 1.novice/tListing/tailwind.config.js
+++ 1.novice/tListing/tailwind.config.js
@@ -1,18 +1,10 @@
 module.exports = {
-  content: ["*.{html}"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-      },
+      fontFamily: {
+        'dosis': ['Dosis', 'sans-serif']
+      }
     },
   },
   plugins: [],
